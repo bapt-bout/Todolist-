6510d2bb291c97943b8848d9a090d8e534a4dc5a
bapt-bout <baptiste.boutari@gmail.com>
2023-02-24 12:20:30 +0100

    init

---
+++ src/components/TodoItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { BiCheckCircle } from "react-icons/bi";
+
+export default function TodoItem(props) {
+  const { todo, removeTodo, completeTodo } = props;
+  return (
+    <div className={todo.completed ? "todo-row complete" : "todo-row"}>
+      {props.todo.text}
+      <div className="iconsContainer">
+        <RiCloseCircleLine
+          className="icon"
+          style={{ marginRight: 5 }}
+          onClick={() => props.removeTodo(todo.id)}
+        />
+        <BiCheckCircle onClick={() => completeTodo(todo.id)} />
+      </div>
+    </div>
+  );
+}
